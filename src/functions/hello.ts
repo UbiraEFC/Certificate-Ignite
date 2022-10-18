557ad5ff0faeda099db857c3184e8edc4a7fa0a0
UbiraEFC <ubirata.efc@gmail.com>
2022-10-18 07:52:23 -0300

    Implements DynamoDB

--- src/functions/hello.ts
+++ src/functions/hello.ts
@@ -6,7 +6,7 @@ export const handler: APIGatewayProxyHandler = async (event) => {
 	return {
 		statusCode: 201,
 		body: JSON.stringify({
-			message: "Hellow World Ignite Serverless",
+			message: "Hello World Ignite Serverless",
 		}),
 	};
 };
